16eddb7f0f26f2248d838a57190c31bf93f028d8
Pawel <p.j.bieniek@wp.pl>
2019-06-23 20:12:42 +0200

    Form validation implemented

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@ function validateForm(){
     let name = document.contact.name.value;
     let email = document.contact.email.value;
     let comments = document.contact.comments.value;
+    let nameRGX = /^[A-Z]{1}[a-z]{1,9}\s[A-Z]{1}[a-z]{1,9}$/;
+    let emailRGX = /^.{1,10}@.{1,10}\..{1,3}$/
 
-    if (name=="" || email==""){
-        alert("You cant send blank form");
+    var nameResult = nameRGX.test(name);
+    var emailResult = emailRGX.test(email);
+
+    if (nameResult == false){
+        alert('first or last name no longer than 10 letters, should start with Upper case')
         return false;
-    } else if (name=="Jan"){
-        alert("Good");
+    } else if (emailResult == false){
+        alert('Invailid email')
         return false;
+    } else {
+        return true;
     }
 }
